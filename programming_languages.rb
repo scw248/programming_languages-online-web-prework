7b4abc59051ac8030000020cf2664903711cd682
Stephen Wright <scw248@gmail.com>
2019-01-10 22:18:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  
+  languages.each do |style, hash|
+    hash.each do |language, type|
+      if language == :ruby
+      type.each do |key, val|
+        binding.pry
+      new_languages[:ruby] = { type: val }
+      h2 = { "style" => style }
+      new_languages.merge(h2)
+    end
+end
+end
+end
 end
