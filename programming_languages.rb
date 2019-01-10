8e42887978c5f3d62069916f4982ea1b8ad88026
Stephen Wright <scw248@gmail.com>
2019-01-10 22:33:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,12 +8,10 @@ def reformat_languages(languages)
       type.each do |key, val|
       new_languages[:ruby] = { key => val }
       h2 = { "style" => style }
-      binding.pry
       new_languages.merge!(h2)
     end
 end
 end
 end
-binding.pry
 new_languages
 end
